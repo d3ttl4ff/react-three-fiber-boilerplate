2814affc762c7bf7c7ba0a06e756f6ced805f8c0
d3ttl4ff <recon.mikasaackerman@gmail.com>
2024-09-25 10:45:11 +0530

    :sparkles: Updated World

--- src/Experience/World/Base.jsx
+++ src/Experience/World/Base.jsx
@@ -1,14 +1,9 @@
 const Base = () => {
   return (
     <>
-      <mesh castShadow position-y={1}>
+      <mesh castShadow scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color={'red'} />
-      </mesh>
-
-      <mesh rotation-x={-Math.PI * 0.5} scale={5}>
-        <planeGeometry />
-        <meshStandardMaterial color={'greenyellow'} />
+        <meshNormalMaterial />
       </mesh>
     </>
   );
